1aa96a92e423d2be20c27f15620df9cccb3a99a5
seonghun120614 <seonghun120614@gmail.com>
2024-11-22 13:54:17 +0900

    feat: Impl. intial Canvas that be able to zoom in/out movement

--- app/src/main/java/com/travelsketch/ui/activity/CanvasActivity.kt
+++ app/src/main/java/com/travelsketch/ui/activity/CanvasActivity.kt
@@ -3,38 +3,47 @@ package com.travelsketch.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 
 class CanvasActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         setContent {
-            Column (
+            val scale = remember { mutableStateOf(1f) }
+            val offsetX = remember { mutableStateOf(0f) }
+            val offsetY = remember { mutableStateOf(0f) }
+
+            Canvas(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(Color.Gray),
-                verticalArrangement = Arrangement.Center
-            ) {
-                Canvas(
-                    modifier = Modifier
-                        .aspectRatio(1.41f)
-                        .fillMaxWidth(),
-                    onDraw = {
-                        drawRect(color = Color.White)
-                    }
-                )
-            }
+                    .graphicsLayer(
+                        scaleX = scale.value,
+                        scaleY = scale.value,
+                        translationX = offsetX.value,
+                        translationY = offsetY.value
+                    )
+                    .pointerInput(Unit) {
+                        detectTransformGestures { _, pan, zoom, _ ->
+                            scale.value *= zoom
+                            offsetX.value += pan.x
+                            offsetY.value += pan.y
+                        }
+                    },
+                onDraw = {
+                    drawRect(
+                        color = Color.White
+                    )
+                }
+            )
         }
     }
 }
