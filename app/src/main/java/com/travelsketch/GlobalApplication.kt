54be92b94515a42771b4a02f3b9eb0200c1104d5
seonghun120614 <seonghun120614@gmail.com>
2024-11-23 17:25:51 +0900

    fix: Delete  for confliction

--- app/src/main/java/com/travelsketch/GlobalApplication.kt
+++ app/src/main/java/com/travelsketch/GlobalApplication.kt
@@ -2,9 +2,7 @@ package com.travelsketch
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
