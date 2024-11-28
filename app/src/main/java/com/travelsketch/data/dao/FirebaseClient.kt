1da19d7351395ee0fdfd071b51a04864cad0b5f7
Park Seonghun <58248012+seonghun120614@users.noreply.github.com>
2024-11-28 14:58:35 +0900

    fix: Update `FirebaseClient.kt`

--- app/src/main/java/com/travelsketch/data/dao/FirebaseClient.kt
+++ app/src/main/java/com/travelsketch/data/dao/FirebaseClient.kt
@@ -87,7 +87,7 @@ object FirebaseClient: DatabaseClient {
         boxData: BoxData
     ): Boolean {
         return try {
-            databaseRef.child("canvas").child(canvasId).setValue(boxData).await()
+            databaseRef.child("canvas").child(canvasId).child(boxId).setValue(boxData).await()
             true
         } catch (e: Exception) {
             Log.d("ITM", "DAO Error: $e")
